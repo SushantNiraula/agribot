259f087dac93d0ab5eec4a1e57e33bbf60417749
SushantNiraula <078bei047@ioepc.edu.np>
2026-01-08 11:28:59 +0545

    made minor changes to agribot.gazebo file with addition of new color gazebo/black for the wheels and sensors. Also made 2 new packages bumperbot_examples_py and bumerbot_examples_cpp for trying and testing concepts i learn.

---
+++ bumperbot_examples_py/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'bumperbot_examples_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sushant-ros',
+    maintainer_email='078bei047@ioepc.edu.np',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
